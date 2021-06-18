1865d9f78eba26aa0e2eab758bb73efb2f6d28e4
edejed <81303023+edejed@users.noreply.github.com>
2021-06-18 21:49:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
 import random
-
 import simpy
 
 
@@ -26,24 +25,24 @@ def customer(env, movie, num_tickets, cinema):
 
         # The customer who's in queue will buy a ticket
         cinema.available[movie] -= num_tickets
-        if  cinema.available[movie] < 2:
-             cinema.sold_out[movie].succeed()
-             cinema.when_sold_out[movie] = env.now
-             cinema.available[movie] = 0
+        if cinema.available[movie] < 2:
+            cinema.sold_out[movie].succeed()
+            cinema.when_sold_out[movie] = env.now
+            cinema.available[movie] = 0
         yield env.timeout(1)
 
 
-def customer_arrivals(env, cinema):
+def customer_arrivals(env, theater):
     while True:
         yield env.timeout(random.expovariate(1 / 0.5))
 
-        movie = random.choice( cinema.movies)
+        movie = random.choice(theater.movies)
         num_tickets = random.randint(1, 6)
-        if  cinema.available[movie]:
-            env.process(customer(env, movie, num_tickets,  cinema))
+        if theater.available[movie]:
+            env.process(customer(env, movie, num_tickets, theater))
 
 
- cinema = collections.namedtuple('cinema', 'counter, movies, available, '
+Theater = collections.namedtuple('Theater', 'counter, movies, available, '
                                             'sold_out, when_sold_out, '
                                             'num_renegers')
 
@@ -60,17 +59,17 @@ available = {movie: tickets for movie in movies}
 sold_out = {movie: env.event() for movie in movies}
 when_sold_out = {movie: None for movie in movies}
 num_renegers = {movie: 0 for movie in movies}
- cinema =  cinema(counter, movies, available, sold_out, when_sold_out,
-                  num_renegers)
+cinema = Theater( counter, movies, available, sold_out, when_sold_out,
+                  num_renegers )
 
 # build and run
-env.process(customer_arrivals(env,  cinema))
+env.process( customer_arrivals( env, cinema ) )
 env.run(until=sim_time)
 
 # Results of data
 for movie in movies:
-    if  cinema.sold_out[movie]:
+    if cinema.sold_out[movie]:
         print('Show "%s" sold out %.1f minutes after ticket counter '
-              'opening.' % (movie,  cinema.when_sold_out[movie]))
+              'opening.' % (movie, cinema.when_sold_out[movie]) )
         print(' %s People leaves the queue after the ticket got sold out' %
-               cinema.num_renegers[movie])
+              cinema.num_renegers[movie] )
